390e798bf89811ea5d1f227f082bfa21a160a46e
Srilakshmi SV <srilakshmi2011.v@gmail.com>
2026-01-06 11:58:47 +0530

    test file

---
+++ src/main/java/com/example/demo/model/Designation.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+public class Designation {
+
+	private String job_title;
+
+	public String getJob_title() {
+		return job_title;
+	}
+
+	public void setJob_title(String job_title) {
+		this.job_title = job_title;
+	}
+}
